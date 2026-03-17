0015793634bbc81076b017d9fcdaa2052703089b
RyanLildhar <ryan.lildhar1@ontariotechu.net>
2026-03-16 22:49:50 -0400

    updated

--- src/main/java/fooddelivery/model/Menu.java
+++ src/main/java/fooddelivery/model/Menu.java
@@ -1,4 +1,4 @@
-package fooddelivery.model;
+package com.fooddelivery.model;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +9,9 @@ public class Menu {
 
     public Menu() {}
 
-    public List<MenuItem> getItems() { return items; }
-    public void setItems(List<MenuItem> items) { this.items = items; }
+    public List<MenuItem> getItems()            { return items; }
+    public void           setItems(List<MenuItem> items) { this.items = items; }
 
-    public void addItem(MenuItem item) {}
-    public void removeItem(String itemId) {}
+    public void addItem(MenuItem item)          { items.add(item); }
+    public void removeItem(String itemId)       { items.removeIf(i -> i.getId().equals(itemId)); }
 }
