b0b978b7d9f89c8cbb7f5febb3d547903b89e088
RyanLildhar <ryan.lildhar1@ontariotechu.net>
2026-03-16 22:51:23 -0400

    updated

--- src/main/java/fooddelivery/model/Order.java
+++ src/main/java/fooddelivery/model/Order.java
@@ -1,36 +1,32 @@
-package fooddelivery.model;
-
-import java.util.ArrayList;
+package com.fooddelivery.model;
 import java.util.List;
 
 public class Order {
 
-    private String orderId;
-    private String customerId;
-    private String restaurantId;
-    private List<MenuItem> items = new ArrayList<>();
-    private double totalCost;
-    private String status;
+    private String         id;
+    private String         customerId;
+    private String         restaurantId;
+    private List<MenuItem> items;
+    private String         status;
+    private double         totalPrice;
 
     public Order() {}
 
-    public String getOrderId() { return orderId; }
-    public void setOrderId(String orderId) { this.orderId = orderId; }
+    public String getId()                   { return id; }
+    public void   setId(String id)          { this.id = id; }
 
-    public String getCustomerId() { return customerId; }
-    public void setCustomerId(String customerId) { this.customerId = customerId; }
+    public String getCustomerId()                       { return customerId; }
+    public void   setCustomerId(String customerId)      { this.customerId = customerId; }
 
-    public String getRestaurantId() { return restaurantId; }
-    public void setRestaurantId(String restaurantId) { this.restaurantId = restaurantId; }
+    public String getRestaurantId()                         { return restaurantId; }
+    public void   setRestaurantId(String restaurantId)      { this.restaurantId = restaurantId; }
 
-    public List<MenuItem> getItems() { return items; }
-    public void setItems(List<MenuItem> items) { this.items = items; }
+    public List<MenuItem> getItems()                    { return items; }
+    public void           setItems(List<MenuItem> items){ this.items = items; }
 
-    public double getTotalCost() { return totalCost; }
-    public void setTotalCost(double totalCost) { this.totalCost = totalCost; }
+    public String getStatus()               { return status; }
+    public void   setStatus(String status)  { this.status = status; }
 
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-
-    public void calculateTotal() {}
+    public double getTotalPrice()               { return totalPrice; }
+    public void   setTotalPrice(double price)   { this.totalPrice = price; }
 }
